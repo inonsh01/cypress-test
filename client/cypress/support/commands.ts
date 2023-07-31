899aee295113fc6a4f00e55ce5b2d37bbceb09be
inonsh01 <inonsh01@gmail.com>
2023-07-31 13:31:38 +0300

    ss

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,15 +1,37 @@
 /// <reference types="cypress" />
-import { comparePdf } from "./script";
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>;
-      comparePdf(filePath1: string, filePath2: string): Chainable<boolean>;
-    }
-  }
-}
-
-Cypress.Commands.add("comparePdf", (filePath1: string, filePath2: string) => {
-  return comparePdf(filePath1, filePath2);
-});
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
