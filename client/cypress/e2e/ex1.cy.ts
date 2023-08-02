91ae60b14413c2c7dfc86c6e39ff6fca7ab9d88b
inonsh01 <inonsh01@gmail.com>
2023-08-02 15:55:20 +0300

    going to end

--- client/cypress/e2e/ex1.cy.ts
+++ client/cypress/e2e/ex1.cy.ts
@@ -1,21 +1,21 @@
 
 //INPUTS
-const FILEPATH = "public/newFile1.pdf";
-const EQ_FILEPATH = "public/newFile2.pdf";
+const FILEPATH = "public/file.pdf";
+const EQ_FILEPATH = "public/eqFile.pdf";
 const NOT_EQ_FILEPATH = "public/notEqFile.pdf";
 
-describe("Compare PDF", () => {
+describe("Exercise 1 - Compare PDF", () => {
     it("Should be able to verify same PDFs", () => {
-        cy.readFile(FILEPATH, "base64").then((file1) => {
-            cy.readFile(EQ_FILEPATH, "base64").then((file2) => {
+        cy.readFile(FILEPATH, "utf-8").then((file1) => {
+            cy.readFile(EQ_FILEPATH, "utf-8").then((file2) => {
                 expect(file1).to.deep.equal(file2);
             });
         });
     });
 
     it("Should be able to verify not same PDFs", () => {
-        cy.readFile(FILEPATH, "base64").then((file1) => {
-            cy.readFile(NOT_EQ_FILEPATH, "base64").then((file2) => {
+        cy.readFile(FILEPATH, "utf-8").then((file1) => {
+            cy.readFile(NOT_EQ_FILEPATH, "utf-8").then((file2) => {
                 expect(file1).to.not.equal(file2);
             });
         });
--- client/cypress/e2e/ex2.cy.ts
+++ client/cypress/e2e/ex2.cy.ts
@@ -1,50 +1,61 @@
 //INPUTS
 const USERNAME = "automatedUser26@example.com";
 const PASSWORD = "4r4nd0mp4ssw0rd";
+const CHEAPEST_PHONE_CLASS_NAME = 'cheapest-phone';
 const CHEAPEST_PHONE = {
   PRICE: Infinity,
   LINK: {} as Element
 }
 
-const CHEAPEST_PHONE_CLASS_NAME = 'cheapest-phone';
-
 // SELECTORS
-const SELECTORS = {
-  WEB_SITE: 'https://www.demoblaze.com/',
+const LOGIN_SELECTORS = {
   LOGIN_BUTTON: "#login2",
   USERNAME_INPUT: "#loginusername",
   PASSWORD_INPUT: "#loginpassword",
   SUBMIT_BUTTON: "#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary",
+}
+
+const CHEAPEST_PHONE_SELECTORS = {
+  WEB_SITE: 'https://www.demoblaze.com/',
   PHONE_CATEGORY: `[onclick="byCat('phone')"]`,
   PHONES_TABLE: "#tbodyid",
   CARD: ".card",
   NEXT_BUTTON: "#next2",
   CHEAPEST_PHONE: "." + CHEAPEST_PHONE_CLASS_NAME,
-  ADD_TO_CART: ".col-sm-12 > .btn"
+  ADD_TO_CART: ".col-sm-12 > .btn",
+  CART: "#cartur",
 };
 
-describe("Login", () => {
+//TESTS
+describe("Exercise 2 - login & add to cart the cheapest phone", () => {
   beforeEach(() => {
-    cy.visit(SELECTORS.WEB_SITE);
+    cy.visit(CHEAPEST_PHONE_SELECTORS.WEB_SITE);
   });
 
   it("should login successfully", () => {
-    cy.get(SELECTORS.LOGIN_BUTTON).click();
+    cy.get(LOGIN_SELECTORS.LOGIN_BUTTON).click();
     cy.wait(500);
-    cy.get(SELECTORS.USERNAME_INPUT).type(USERNAME);
-    cy.get(SELECTORS.PASSWORD_INPUT).type(PASSWORD);
-    cy.get(SELECTORS.SUBMIT_BUTTON).click();
+    cy.get(LOGIN_SELECTORS.USERNAME_INPUT).type(USERNAME);
+    cy.get(LOGIN_SELECTORS.PASSWORD_INPUT).type(PASSWORD);
+    cy.get(LOGIN_SELECTORS.SUBMIT_BUTTON).click();
   });
 
   it("should find the cheapest phone and add it to cart", () => {
-    cy.get(SELECTORS.PHONE_CATEGORY).click().then(() => {
+    //Enter to phones category
+    cy.get(CHEAPEST_PHONE_SELECTORS.PHONE_CATEGORY).click().then(() => {
       cy.wait(500)
+
+      //Find the cheapest phone in the list
       findMinElement();
       cy.wait(500).then(() => {
-        CHEAPEST_PHONE.LINK.classList.add(CHEAPEST_PHONE_CLASS_NAME);
-        cy.get(SELECTORS.CHEAPEST_PHONE).click();
-        cy.get(SELECTORS.ADD_TO_CART).click();
 
+        //Add className to the cheapest phone link and click on it.
+        CHEAPEST_PHONE.LINK.classList.add(CHEAPEST_PHONE_CLASS_NAME);
+        cy.get(CHEAPEST_PHONE_SELECTORS.CHEAPEST_PHONE).click();
+
+        //Add the cheapest phone to cart and get in it.
+        cy.get(CHEAPEST_PHONE_SELECTORS.ADD_TO_CART).click();
+        cy.get(CHEAPEST_PHONE_SELECTORS.CART).click();
       })
     });
   });
@@ -53,15 +64,21 @@ describe("Login", () => {
 export { };
 
 function findMinElement() {
-  cy.get(SELECTORS.PHONES_TABLE).then(($phonesList) => {
+  //Get the list of all the phones
+  cy.get(CHEAPEST_PHONE_SELECTORS.PHONES_TABLE).then(($phonesList) => {
     $phonesList.each((index, $li) => {
       cy
         .wrap($li)
-        .get(SELECTORS.CARD)
+        .get(CHEAPEST_PHONE_SELECTORS.CARD)
         .then((cards) => {
           for (let card of cards) {
+            //Get the href of the phone card
             const cheapestPhoneLink = card.children[1].children[0].children[0];
+
+            //Get the price of the phone card
             const cheapestPrice = Number(card.children[1].children[1].innerHTML.split('$')[1]);
+
+            //Save the cheapest phone card by price
             if (cheapestPrice < CHEAPEST_PHONE.PRICE) {
               CHEAPEST_PHONE.PRICE = cheapestPrice;
               CHEAPEST_PHONE.LINK = cheapestPhoneLink;
